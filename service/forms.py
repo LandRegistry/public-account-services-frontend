c3ce85dc13c16ea95550de736e96ec5b1de554b1
vagrant <dmrwooding@gmail.com>
2016-01-19 16:15:33 +0000

    Added functionality to AC form

--- service/forms.py
+++ service/forms.py
@@ -1,5 +1,5 @@
 from flask.ext.wtf import Form
-from wtforms import StringField, BooleanField
+from wtforms import StringField, BooleanField, PasswordField
 from wtforms.validators import DataRequired
 
 class UserAccountForm(Form):
@@ -9,13 +9,16 @@ class UserAccountForm(Form):
 	business = StringField('business')
 	postcode = StringField('postcode', validators=[DataRequired()])
 	address1 = StringField('address1', validators=[DataRequired()])
-	address2 = StringField('address2')
-	address3 = StringField('address3')
-	address4 = StringField('address4')
+	address2 = StringField('address2', validators=[DataRequired()])
 	city = StringField('city', validators=[DataRequired()])
 	county = StringField('county')
 	country = StringField('country', validators=[DataRequired()])
 	payment_address = BooleanField('payment_address', default=True)
+	ppostcode = StringField('ppostcode', validators=[DataRequired()])
+	paddress1 = StringField('paddress1', validators=[DataRequired()])
+	paddress2 = StringField('paddress2', validators=[DataRequired()])
+	pcity = StringField('pcity', validators=[DataRequired()])
+	pcountry = StringField('pcity', validators=[DataRequired()])
 	email1 = StringField('email1', validators=[DataRequired()])
 	email2 = StringField('email2', validators=[DataRequired()])
 	telephone = StringField('telephone')
@@ -23,5 +26,5 @@ class UserAccountForm(Form):
 	preference_phone = BooleanField('preference_phone', default=True)
 	preference_post = BooleanField('preference_post', default=True)
 	login_username = StringField('login_username', validators=[DataRequired()])
-	login_password1 = StringField('login_password1', validators=[DataRequired()])
-	login_password2 = StringField('login_password2', validators=[DataRequired()])
+	login_password1 = PasswordField('login_password1', validators=[DataRequired()])
+	login_password2 = PasswordField('login_password2', validators=[DataRequired()])
